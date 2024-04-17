0c920873e7d280cec0644d7024c248026d05eec2
Thiago Henrique <153330691+Arthiagx@users.noreply.github.com>
2024-04-17 11:19:12 -0300

    Estilizando cards com pokemons.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,8 @@
 import { onMounted, reactive, ref } from "vue";
 import ListPokemons from "../components/ListPokemons.vue";
 
+let urlBaseSvg = ref("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/")
+
 let pokemons = reactive(ref());
 
 onMounted (() => {
@@ -25,17 +27,21 @@ onMounted (() => {
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div> -->
-      </div>
+          </div>
+      </div> -->
         </div>
         <div class="col-sm-12 col-md-6">
-          <ListPokemons
-          v-for="pokemon in pokemons"
-          :key="pokemon.name"
-          :name="pokemon.name"
-          />
-        
-        </div>      
+          <div class="card">
+            <div class="card-body row">
+              <ListPokemons
+                v-for="pokemon in pokemons"
+                :key="pokemon.name"
+                :name="pokemon.name"
+                :urlBaseSvg="urlBaseSvg + pokemon.url.split('/')[6] + '.svg'"
+                />
+            </div>          
+        </div>
+      </div>      
     </div>     
     
     </div>
